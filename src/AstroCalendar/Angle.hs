be005f940d05b2ca1659466dec71a3185f65984a
Kierán Meinhardt <kmein@posteo.de>
2025-03-15 23:45:00 +0100

    remove unused mipoint calculation

--- src/AstroCalendar/Angle.hs
+++ src/AstroCalendar/Angle.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE ViewPatterns #-}
 
-module AstroCalendar.Angle (Angle (..), difference, degreesMinutes, midpoint) where
+module AstroCalendar.Angle (Angle (..), difference, degreesMinutes) where
 
 import Data.Aeson (ToJSON)
 import Data.Fixed (mod')
@@ -21,22 +21,3 @@ difference angle1 angle2 =
 
 degreesMinutes :: Angle -> (Int, Int)
 degreesMinutes (degrees -> a) = (truncate a, round $ (a - fromInteger (truncate a)) * 60)
-
-radians :: Angle -> Double
-radians x = degrees x * pi / 180
-
-fromRadians :: Double -> Angle
-fromRadians x = Angle (x * 180 / pi)
-
-midpoint :: Angle -> Angle -> Angle
-midpoint a b =
-  let -- Convert longitudes to Cartesian coordinates
-      (x1, y1) = (cos (radians a), sin (radians a))
-      (x2, y2) = (cos (radians b), sin (radians b))
-
-      -- Average Cartesian coordinates
-      (xMid, yMid) = ((x1 + x2) / 2, (y1 + y2) / 2)
-
-      -- Convert back to longitude (handling quadrant issues)
-      midLng = mod' (fromRadians (atan2 yMid xMid)) 360
-   in midLng
