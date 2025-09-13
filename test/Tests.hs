b605262cdfd98a26ad38a9ed1f9ba01e1902626b
Kierán Meinhardt <kmein@posteo.de>
2025-09-13 12:29:18 +0200

    add test infrastructure

---
+++ test/Tests.hs
@@ -0,0 +1,45 @@
+module Main where
+
+import Data.Aeson
+import AstroCalendar.Ephemeris
+import AstroCalendar.Types
+import AstroCalendar.Chart
+import AstroCalendar.Aspect
+import Data.Maybe (fromJust)
+import SwissEphemeris (GeographicPosition(..))
+import Test.Tasty
+import Test.Tasty.HUnit
+import Data.Time.Format (parseTimeM, defaultTimeLocale)
+import Data.Time (UTCTime)
+
+main :: IO ()
+main = defaultMain tests
+
+
+parseISO8601 :: String -> Maybe UTCTime
+parseISO8601 isoString =
+    parseTimeM True defaultTimeLocale "%Y-%m-%d %H:%M" isoString :: Maybe UTCTime
+
+tests :: TestTree
+tests = testGroup "All Tests" [birthCharts]
+
+birthCharts :: TestTree
+birthCharts =
+  testGroup
+    "Birth charts"
+    [ testCase "Goethe's birth chart" $ do
+        -- https://www.astro-seek.com/birth-chart/johann-wolfgang-von-goethe-horoscope
+        let selectionOptions =
+              SelectionOptions
+                { planetSelection = ModernPlanets,
+                  aspectTypeSelection = AllAspectTypes,
+                  orbSelection = AstroDienst,
+                  position = Just GeographicPosition {geoLat = 50.12, geoLng = 8.68 }
+                }
+            utcTime = fromJust $ parseISO8601 "1749-08-28 11:55"
+        chart <- natalChart selectionOptions utcTime
+        let aspects = findAspects selectionOptions chart
+        let gotJson = chartJson [chart] aspects
+        neededJson <- decodeFileStrict "test_data/goethe_chart.json" :: IO (Maybe Value)
+        neededJson @=? Just gotJson
+    ]
